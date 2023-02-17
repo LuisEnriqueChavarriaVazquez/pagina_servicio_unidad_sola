aedbbfd3552c8d93adf944d855d2367f6e893bb6
luis <luisechvz@gmail.com>
2023-02-17 15:39:43 -0600

    Correciones al menu lateral

--- static/js/menuLateral.js
+++ static/js/menuLateral.js
@@ -1,20 +1,20 @@
-let containerMenuAndContent = document.getElementsByClassName('containerMenuAndContent');
+let containerMenuAndContent = document.getElementsByClassName('containerMenuAndContentClose');
 let containerMenuAndContentPrimeraSeccion = document.getElementsByClassName('containerMenuAndContent-primeraSeccion');
 let botonFlotante1 = document.getElementById('botonFlotante1');
 let botonFlotanteIcon1 = document.getElementById('botonFlotanteIcon1');
 
-let menuState = "open";
+let menuState = "close";
 
 botonFlotante1.addEventListener('click', () => {
     if(menuState == "open"){
         containerMenuAndContentPrimeraSeccion[0].classList.add('hideElement');
-        containerMenuAndContent[0].classList.add('containerMenuAndContentClose');
+        containerMenuAndContent[0].classList.add('containerMenuAndContent');
         botonFlotanteIcon1.classList.remove('bi-x');
         botonFlotanteIcon1.classList.add('bi-list');
         menuState = "close";
     }else{
         containerMenuAndContentPrimeraSeccion[0].classList.remove('hideElement');
-        containerMenuAndContent[0].classList.remove('containerMenuAndContentClose');
+        containerMenuAndContent[0].classList.remove('containerMenuAndContent');
         botonFlotanteIcon1.classList.add('bi-x');
         botonFlotanteIcon1.classList.remove('bi-list');
         menuState = "open";
