5fbae1c16ce0fa67541a88f1a36ef9b76fe5f48f
luis <luisechvz@gmail.com>
2023-03-03 02:56:39 -0600

    Buscador del glosario

---
+++ static/js/buscador.js
@@ -0,0 +1,39 @@
+function buscar_concepto(){
+    let criterio=document.getElementById('inputBrowser').value; 
+    criterio=criterio.toLowerCase();
+    let items=$('.conceptCard');
+    var item;
+
+    let fullContainer = document.getElementById('fullContainer');
+    let contadorPalabras = 0;
+
+    let cardContainer = $('.cardContainer');
+    for(item=0; item <items.length; item++){
+        if (!items[item].innerHTML.toLowerCase().includes(criterio)) {
+            cardContainer[item].style.display="none";
+        }
+        else {
+            cardContainer[item].style.display="grid";
+            contadorPalabras = 1;          
+        }
+    }
+
+    if(contadorPalabras == 0){
+        fullContainer.innerHTML = `
+        <section class="contenedormaterialDeApoyo">
+            <p class="fs-1 fw-bold">No hay coincidencias</p>
+            <div class="imagenDefaultContainer mt-3">
+                <img class="imagenDefault" src="../static/img/default.png">
+            </div>
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                    <div class="d-grid gap-2">
+                        <a href="/glosario" class="btn btn-secondary btn-lg">Reiniciar</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        `;
+    }
+
+}
