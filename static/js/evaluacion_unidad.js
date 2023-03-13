638bbb2b34a56e739887140e3d2631d24c4e9871
UlisesJE <ujuareze1700@alumno.ipn.mx>
2023-03-12 23:23:30 -0600

    generando preguntas aleatorias al cargar la pagina, con su propuesta de evaluación

--- static/js/evaluacion_unidad.js
+++ static/js/evaluacion_unidad.js
@@ -14,104 +14,86 @@ function pregunta(numero) {
 }
 
 function preguntas_aleatorias() {
-    let contenedor_preguntas = document.getElementById('contenedor_preguntas');
-    const preguntas_aleatorias_indices=[]
+    
+    const preguntas_aleatorias_indices=[0]
     const preguntas = [
         0,
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 1",
+            "opciones":[123,2334,434,45],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 2",
+            "opciones":[123,2334,434,567],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 3",
+            "opciones":[123,2334,434,45],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 4",
+            "opciones":[123,2334,434,56],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 5",
+            "opciones":[123,2334,434,56],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 6",
+            "opciones":[123,2334,434,56],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 7",
+            "opciones":[123,2334,434,56],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 8",
+            "opciones":[123,2334,434,56],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 9",
+            "opciones":[123,2334,434,563],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 10",
+            "opciones":[123,2334,434,345],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 11",
+            "opciones":[123,2334,434,3456],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 12",
+            "opciones":[123,2334,434,3541],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 13",
+            "opciones":[123,2334,434],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 14",
+            "opciones":[123,2334,434,5655],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 15",
+            "opciones":[123,2334,434,455],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 16",
+            "opciones":[123,2334,434,3434],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 17",
+            "opciones":[123,2334,434,452],
         },
         {
-            "enunciado": "Molecule Man",
-            "opciones":[0,123,2334,434],
-            "respuesta": 2
+            "enunciado": "pregunta 18",
+            "opciones":[123,2334,434,3434],
         }
     ]
 
 
-    while(preguntas_aleatorias_indices.length<10){
+    while(preguntas_aleatorias_indices.length<11){
         numero=aleatorio(1,preguntas.length);
         if (preguntas_aleatorias_indices.includes(numero)==false) {
             preguntas_aleatorias_indices.push(numero)
@@ -119,41 +101,41 @@ function preguntas_aleatorias() {
     }
     
 
-    for(i=0;i<preguntas_aleatorias_indices.length;i++){
-        console.log(preguntas_aleatorias_indices[i])
-        contenedor_preguntas.innerHTML = `
+    let contenedor_preguntas = document.getElementById('contenedor_preguntas');
+    let contenido_preguntas="";
+    for(i=1;i<preguntas_aleatorias_indices.length;i++){
+        contenido_preguntas = contenido_preguntas+ `
         <div class="card">
-        <div class="card-body" id="pregunta1">
-            <h5 class="card-title">Pregunta 1</h5>
-            <p>¿Rerum lorem dldemsds kmsdkndsk sdmsd dsjnsdjn nemo neque voluptatem laboriosam alias
-                itaque ducimus fugiat?</p>
+        <div class="card-body" id="pregunta${preguntas_aleatorias_indices[i]}">
+            <h5 class="card-title">Pregunta ${preguntas_aleatorias_indices[i]}</h5>
+            <p>${preguntas[preguntas_aleatorias_indices[i]].enunciado}</p>
             <section class="m-3" >
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault1"
-                        id="pregunta1_opt1">
-                    <label class="form-check-label" for="pregunta1_opt1">
-                        Opción 1
+                        id="pregunta${preguntas_aleatorias_indices[i]}_opt1">
+                    <label class="form-check-label" for="pregunta${preguntas_aleatorias_indices[i]}_opt1">
+                    ${preguntas[preguntas_aleatorias_indices[i]].opciones[0]}
                     </label>
                 </div>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault1"
-                        id="pregunta1_opt2">
-                    <label class="form-check-label" for="pregunta1_opt2">
-                        Opción 2
+                        id="pregunta${preguntas_aleatorias_indices[i]}_opt2">
+                    <label class="form-check-label" for="pregunta${preguntas_aleatorias_indices[i]}_opt2">
+                    ${preguntas[preguntas_aleatorias_indices[i]].opciones[1]}
                     </label>
                 </div>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault1"
-                        id="pregunta1_opt3">
-                    <label class="form-check-label" for="pregunta1_opt3">
-                        Opción 3
+                        id="pregunta${preguntas_aleatorias_indices[i]}_opt3">
+                    <label class="form-check-label" for="pregunta${preguntas_aleatorias_indices[i]}_opt3">
+                    ${preguntas[preguntas_aleatorias_indices[i]].opciones[2]}
                     </label>
                 </div>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault1"
-                        id="pregunta1_opt4">
-                    <label class="form-check-label" for="pregunta1_opt4">
-                        Opción 4
+                        id="pregunta${preguntas_aleatorias_indices[i]}_opt4">
+                    <label class="form-check-label" for="pregunta${preguntas_aleatorias_indices[i]}_opt4">
+                    ${preguntas[preguntas_aleatorias_indices[i]].opciones[3]}
                     </label>
                 </div>
             </section>
@@ -161,22 +143,19 @@ function preguntas_aleatorias() {
 
             </div>
             <div class="d-grid gap-2">
-                <button class="btn btn-primary" onclick="pregunta(1)" type="button">Evaluar</button>
+                <button class="btn btn-primary" onclick="pregunta(${preguntas_aleatorias_indices[i]})" type="button">Evaluar</button>
             </div>
         </div>
     </div>
         `;
     }
+    contenedor_preguntas.innerHTML=contenido_preguntas
 
 }
 
 function aleatorio(a,b) {
     var aleatorio = Math.round(Math.random()*(b-a)+parseInt(a));
     return aleatorio
-}
-
-function crear_pregunta(){
-    
 }
 
 preguntas_aleatorias();
