2f7622a57ec53b384489d9bea4ef43574930a36d
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-03-14 03:24:20 -0600

    Sistema anticopia

---
+++ static/js/noCopy.js
@@ -0,0 +1,13 @@
+let contenedorNotificaciones = document.getElementById('contenedorNotificaciones');
+document.addEventListener("selectstart", () => {
+    contenedorNotificaciones.innerHTML = `
+        <div id="notificacionMine">
+            No está permitido copiar contenido.
+        </div>
+    `;
+
+    setTimeout(() => {
+        $("#notificacionMine").fadeOut("slow");
+    }, 2000);
+
+});
