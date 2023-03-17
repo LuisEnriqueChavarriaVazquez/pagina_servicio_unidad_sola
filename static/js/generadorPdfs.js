ad7218a46497020ef8b4e579fdc029bad0bd9892
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-03-17 05:33:04 -0600

    PDF actualizado

--- static/js/generadorPdfs.js
+++ static/js/generadorPdfs.js
@@ -88,6 +88,8 @@ generarDocumento.addEventListener('click', () => {
         colores -= 4;
     }
 
-    doc.save('a4.pdf');
-    window.open(doc.output('bloburl'))
+    //doc.save('a4.pdf');
+    //window.open(doc.output('bloburl'))
+    let previewPDF = document.getElementById('previewPDF');
+    previewPDF.setAttribute('src', doc.output('bloburl'));
 })
